f336ba8b5eb3f13eb0459c022dc9d53b3d15de91
leen <leench@gmail.com>
2026-09-06 12:05:03 +0800

    feat: enforce a single desktop instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,9 +7,18 @@ pub mod protocol;
 
 use std::sync::{Arc, Mutex};
 
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .manage(Arc::new(Mutex::new(commands::AppState::default())))
         .invoke_handler(tauri::generate_handler![
             commands::list_devices,
